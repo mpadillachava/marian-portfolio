144edbcea7d856f65bb2a3d6b12827db7368862b
marjo <marianjpadilla@gmail.com>
2023-12-04 03:23:11 -0500

    Commit Message:
    Configure Django for 'myporfolio' project and update models, templates, and static files
    
    This commit configures Django for the 'myporfolio' project updating the different parts for better functioning. It updates the .iml file to correctly define django facets and template configurations. The models.py is also updated to extend the Job model by adding a description field. The home.html template is significantly refactored, removing unnecessary bootstrap templates and configuring it to properly load static files and display job objects from a database. The settings.py file is updated to correctly define paths of static and media files. Some unused file(color-modes.js) is also removed in this commit. These all changes are useful in improving the overall functionality and layout of the portfolio page as well as the handling of static and media files.

--- jobs/models.py
+++ jobs/models.py
@@ -1,6 +1,11 @@
 from django.db import models
 
+
 # Create your models here.
 class Job(models.Model):
     image = models.ImageField(upload_to='images/')
     summary = models.CharField(max_length=200)
+    description = models.CharField(max_length=400, null=True)
+
+    def __str__(self):
+        return self.summary
